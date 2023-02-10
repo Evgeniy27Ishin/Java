ec9920dbc947cd67d3592b4f914e0188fe481782
Evgeniy <97873677+Evgeniy27Ishin@users.noreply.github.com>
2023-02-10 15:19:16 +0700

    delete

--- Decorator/Espresso.java
+++
@@ -1,9 +0,0 @@
-public class Espresso extends Beverage {
-    public Espresso() {
-        description = "Espresso Coffee";
-    }
-
-    public double cost() {
-        return 52.89;
-    }
-}
--- Decorator/HouseBend.java
+++
@@ -1,9 +0,0 @@
-public class HouseBend extends Beverage {
-    public HouseBend() {
-        description = "House Blend Coffee";
-    }
-
-    public double cost() {
-        return 1.89;
-    }
-}
